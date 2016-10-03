32cf727780dcd23a33d73482461fc98940b0050a
codeofcolors <lily05131@hotmail.com>
2016-10-03 15:47:14 -0500

    jquery code challenge

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+var numOfClicks = 0;
+
+$(document).ready(function() {
+  $('.color-button').on("click", function() {
+      numOfClicks = numOfClicks + 1;
+    $('.color-button').html(numOfClicks);
+
+
+
+$(".color-button").on("click", function() {
+    $('.color-button').css("background", "red");
+});
+
+  });
+
+$('.color-button').click(function() {
+    if ($('.color-button' == '#red')) {
+
+
+      }
+
+});
+
+});
