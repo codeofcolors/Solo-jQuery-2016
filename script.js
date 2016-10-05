c026e3af17e196f5b23e9c50aa76d2de0caf6d25
codeofcolors <lily05131@hotmail.com>
2016-10-04 19:44:21 -0500

    final update

--- script.js
+++ script.js
@@ -1,49 +1,31 @@
-var numOfClicks = 0;
-// var red;
-// var yellow;
-// var green;
-// var blue;
+var redClicks = 0;
+var yellowClicks = 0;
+var greenClicks = 0;
+var blueClicks = 0;
 
 $(document).ready(function() {
 
-
-  $('.color-button').first().on("click", function() {
-    numOfClicks = numOfClicks+1;
-  $('#red').html('Total red:' + numOfClicks);
-      $('color-cube').data('data-color', 'red');
-      $('div').css("background-color", "red");
-      $('div').addClass("color-cube");
-
-});
-
+    $('.color-button').first().on("click", function() {
+        $('#red').html('Total red: ' + (redClicks += 1));
+        var redDiv = $('<div class="color-cube red"></div>');
+        $('.container').append(redDiv);
+    });
 
     $('.color-button').eq(1).on("click", function() {
-      numOfClicks = numOfClicks+1;
-      $('#yellow').html('Total yellow' + numOfClicks);
-      $('color-cube').data('data-color', 'yellow');
-      $('div').css("background-color", "yellow");
-        $('div').addClass("color-cube");
-
-
+        $('#yellow').html('Total yellow: ' + (yellowClicks += 1));
+        var yellowDiv = $('<div class="color-cube yellow"></div>');
+        $('.container').append(yellowDiv);
     });
 
     $('.color-button').eq(2).on("click", function() {
-      numOfClicks = numOfClicks + 1;
-    $('#green').html('Total Green' + numOfClicks);
-      $('color-cube').data('data-color', 'green');
-      $('div').css("background-color", "green");
-        $('div').addClass("color-cube");
+        $('#green').html('Total Green: ' + (greenClicks += 1));
+        var greenDiv = $('<div class="color-cube green"></div>');
+        $('.container').append(greenDiv);
     });
 
     $('.color-button').eq(3).on("click", function() {
-      numOfClicks = numOfClicks + 1;
-    $('#blue').html('Total blue' + numOfClicks);
-      $('color-cube').data('data-color', 'blue');
-      $('div').css("background-color", "blue");
-        $('div').addClass("color-cube");
-
+        $('#blue').html('Total blue: ' + (blueClicks += 1));
+        var blueDiv = $('<div class="color-cube blue"></div>');
+        $('.container').append(blueDiv);
     });
-
-
-
-  });
+});
